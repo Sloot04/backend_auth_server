5d83754858d84a960f1b8909d621c474fabd5e09
Suyai Slootmans <suyaislootmans@gmail.com>
2021-09-22 10:03:56 -0300

    routes - controllers

---
+++ controllers/auth.js
@@ -0,0 +1,14 @@
+const { response } = require('express');
+
+const googleAuthController = (req, res = response) => {
+    const token = req.body.token
+
+    res.json({
+        ok: true,
+        token
+    });
+}
+
+module.exports = {
+    googleAuthController
+}
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 
 const app = express();
 
+//body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Rutas
+app.use('/', require('./routes/auth'));
+
 app.listen(process.env.PORT || 3000, () => {
     const port = process.env.PORT || 3000;
     console.log(`Servidor coriendo en el puerto ${port}`);
